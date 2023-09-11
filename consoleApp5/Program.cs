629446f86aeaec2f32eb0a9a5719b994a68eafe1
fatima369jad <143130179+fatima369jad@users.noreply.github.com>
2023-09-11 09:58:14 +0200

    Add project files.

---
+++ consoleApp5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace consoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello ,Cloud23 !");
+        }
+    }
+}
